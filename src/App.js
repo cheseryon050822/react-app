fe78daef14e28e039caa75acb2e8755c6dab639d
cheseryon050822 <cseryon@gmail.com>
2020-12-26 18:53:08 +0900

    12/26.2020

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
         </p>
         <a
           className="App-link"
@@ -15,8 +14,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
         </a>
+
+        <p><button type="button" name="name" value="value" onClick="disp()">helloworld</button></p>
       </header>
     </div>
   );
