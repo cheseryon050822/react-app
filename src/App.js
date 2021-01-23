97c300bdaf0a502ccd41613780046ef96a9e0252
cheseryon050822 <cseryon@gmail.com>
2021-01-23 18:52:41 +0900

    joruno-jobana

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import React from 'react';　
+import Card from './Card';
+
 
 const App= () =>{
   const Name = 'seryon'
   const[count,setCount] = useState(0);
+  const[word,setword]= useState("seryon");
 
   const increment　= () => {
     setCount(count+1);
@@ -14,20 +18,26 @@ const App= () =>{
     setCount(count-1);
     console.log(count);
   };
-  
- 
- 
-  return (
+  const eirin  = () =>{
+    setCount(0);
+    console.log(count);
+  }
+  const Lotte = () => {
+    setword(word+"seryon");
+    console.log(word);
+  }
    
+  return (
+
 <div>
   <h1>Hello world</h1>
   <h2>Hello world</h2>
   <h3>Hello world</h3>
   <h4>Hello world</h4>
-
-<button
-  onClick={() => window.alert("Hello world")}
-  ></button>
+  <Card/>
+<button onClick={() => window.alert("Hello world")}>
+  結ンデ開イテ羅刹ト骸
+  </button>
 
 
 <input type="text"/>
@@ -41,7 +51,14 @@ const App= () =>{
      <button onClick={decrement}>
        よくないね！
      </button>
-     {count}
+     <button onClick={eirin}>
+       リセット
+     </button>
+      {count}
+     <button onClick={Lotte}>
+       ADD  
+     </button>
+     {word}
    </div>
 </div>
   );
