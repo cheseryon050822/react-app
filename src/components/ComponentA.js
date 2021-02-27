ced788ffa9f21c9dccb085be9d405a35d75c3b1e
cheseryon050822 <cseryon@gmail.com>
2021-02-27 18:58:07 +0900

    thunderbird

--- src/components/ComponentA.js
+++ src/components/ComponentA.js
@@ -1,23 +1,55 @@
-import React from 'react'
-import {Link, useHistory} from 'react-router-dom'
-import {Button,Table}from 'react-bootstrap';
+import React ,{useState,useEffect}from 'react'
+import {Link, } from 'react-router-dom'
+import {Table}from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios'
 
 const ComponentA = () => {
-    const history = useHistory ()
-    const joumon = () =>{
-       history.push("/ComponentB")
-    }
-const soviet = () =>{
-  history.push("/")
-}
+    const [count,setCount] = useState();
+    const [data,setdata]=useState();
+useEffect(() => {
+    console.log('空条徐倫');
+
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then(res =>{
+setdata (res.data)
+        console.log(res,'res check')
+    })
+},[]);
+
     return(
     <div>
         <div>ComponentA</div>
         <Link to="ComponentB">ComponentBへ移動</Link>
-        <button onClick={joumon}>Ｂはこちら</button>
-        <Button variant="primary">Hello world</Button>
-        
+        <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>     
     </div>
     );
 };
